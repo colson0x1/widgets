fccb2de0a1251ea565645e3da7d5011e3176eb07
Colson <colsoncalifornication@gmail.com>
2024-03-20 23:09:22 +0545

    use axios to send post request to translate api

--- src/components/LanguageTranslate.jsx
+++ src/components/LanguageTranslate.jsx
@@ -69,7 +69,7 @@ const Translate = () => {
       />
       <hr />
       <h3 className='ui header'>Output</h3>
-      <Translate text={text} language={language} />
+      <Convert text={text} language={language} />
     </div>
   );
 };
