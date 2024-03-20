85033ced1d23838ac03a2c4d025a10fe840537a7
Colson <colsoncalifornication@gmail.com>
2024-03-20 15:06:37 +0545

    use useRef hook to detect which element was clicked

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,11 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
+  const ref = useRef();
 
   useEffect(() => {
-    document.body.addEventListener('click', () => {
-      console.log('BODY CLICK!');
+    document.body.addEventListener('click', (event) => {
+      // console.log(event.target);
+      if (ref.current.contains(event.target)) {
+        return;
+      }
+
       setOpen(false);
     });
   }, []);
@@ -19,25 +24,21 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
       <div
         key={option.value}
         className='item'
-        onClick={() => {
-          console.log('ITEM CLICKED');
-          onSelectedChange(option);
-        }}
+        onClick={() => onSelectedChange(option)}
       >
         {option.label}
       </div>
     );
   });
 
+  // console.log(ref.current);
+
   return (
-    <div className='ui form'>
+    <div ref={ref} className='ui form'>
       <div className='field'>
         <label className='label'>Select a Color</label>
         <div
-          onClick={() => {
-            console.log('DROPDOWN CLICKED');
-            setOpen(!open);
-          }}
+          onClick={() => setOpen(!open)}
           className={`ui selection dropdown ${open ? 'visible active' : ''}`}
         >
           <i className='dropdown icon'></i>
