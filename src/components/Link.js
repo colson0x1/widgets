8625bbb5cbc251099827f8ba8472c166f00a6968
Colson <colsoncalifornication@gmail.com>
2024-03-21 09:51:28 +0545

    change url without full page refresh + emit navigation event when user clicks on Link + update some state in route component anytime url gets changed

--- src/components/Link.js
+++ src/components/Link.js
@@ -3,6 +3,13 @@ import React from 'react';
 const Link = ({ className, href, children }) => {
   const onClick = (event) => {
     event.preventDefault();
+    // change url without full page refresh
+    window.history.pushState({}, '', href);
+
+    // emit navigation event when user clicks on Link
+    // i.e This communicates to the Route component that the URL has just changed
+    const navEvent = new PopStateEvent('popstate');
+    window.dispatchEvent(navEvent);
   };
 
   return (
--- src/components/Route.js
+++ src/components/Route.js
@@ -1,5 +1,32 @@
+import { useEffect, useState } from 'react';
+
 const Route = ({ path, children }) => {
-  return window.location.pathname === path ? children : null;
+  // we're going to create a piece of state, that essentially follows or tracks
+  // whatever the value of window.location.pathname is
+  // i.e We're essentially introducing a piece of state that is just has the
+  // soul purpose of trying to get the Route component to rerender itself.
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+  useEffect(() => {
+    const onLocationChange = () => {
+      // Every single time, we console log, we get 4 copies of the Route component
+      // and each of them is listening to the navigation event
+      // console.log('Location Change');
+
+      setCurrentPath(window.location.pathname);
+    };
+
+    // Listen to the event emitted by Link
+    window.addEventListener('popstate', onLocationChange);
+
+    // If we ever decide to stop showing the Route component on the screen,
+    // we would want to make sure that we clean up this event listener
+    return () => {
+      window.removeEventListener('popstate', onLocationChange);
+    };
+  }, []);
+
+  return currentPath === path ? children : null;
 };
 
 export default Route;
